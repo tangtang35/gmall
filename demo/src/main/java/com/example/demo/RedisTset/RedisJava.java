bcaa249869b07adf068649875e024a5428812147
tangtang35 <13005919430@163.com>
2020-10-15 10:20:56 +0800

    Redis相关操作

--- demo/src/main/java/com/example/demo/RedisTset/RedisJava.java
+++ demo/src/main/java/com/example/demo/RedisTset/RedisJava.java
@@ -7,7 +7,7 @@ public class RedisJava {
         //连接本地的 Redis 服务
         Jedis jedis = new Jedis("172.19.1.123",6379);
         // 如果 Redis 服务设置来密码，需要下面这行，没有就不需要
-        //jedis.auth("Ankki@2009");
+        jedis.auth("Ankki@2009");
         System.out.println("连接成功");
         //查看服务是否运行
         System.out.println("服务正在运行: "+jedis.ping());
